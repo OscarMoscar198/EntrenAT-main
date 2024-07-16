e206b35af0e297e3fae09e7e93ca471f8eab4cb6
Oscar Castañeda <109644116+OscarMoscar198@users.noreply.github.com>
2024-07-15 23:18:42 -0600

    SplashScreen try 1

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useEffect, useCallback } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { WelcomeScreen } from "./src/ui/screens/WelcomeScreen";
@@ -9,10 +9,21 @@ import { PaperProvider } from "react-native-paper";
 import HomeScreen from "./src/ui/screens/HomeScreen";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const hideSplashScreen = useCallback(async () => {
+    await SplashScreen.hideAsync();
+  }, []);
+
+  useEffect(() => {
+    hideSplashScreen();
+  }, [hideSplashScreen]);
+
   return (
     <PaperProvider>
       <NavigationContainer>
